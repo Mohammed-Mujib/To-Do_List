78a7da15b419c6c6c3a656093a31229e73fc3b85
Mohammed-Mujib <wadmujib@gmail.com>
2024-03-30 18:13:27 +0200

    version 1.0.1

--- js/todo.js
+++ js/todo.js
@@ -9,19 +9,23 @@ function saveData() {
 }
 function addTask() {
     if (inputBox.value=== "") {
+        //TODO:make the alert more cooler
         alert('you must write something');
     }
     else{
+        //FIXME: dont let the text go out of the border
         let li = document.createElement("li");
         let check_icon = document.createElement("i");
         let del_icon = document.createElement("i");
-        let li_text =document.createTextNode(inputBox.value);
+        let par = document.createElement("p")
+        let p_text =document.createTextNode(inputBox.value);
         check_icon.className = `fa-regular fa-square`;
         check_icon.classList.add("check_box");
         del_icon.className = `fa-solid fa-trash`;
         del_icon.classList.add("delete");
+        par.appendChild(p_text)
         li.appendChild(check_icon);
-        li.appendChild(li_text);
+        li.appendChild(par);
         li.appendChild(del_icon);
         listContainer.appendChild(li);
         inputBox.value = null;
@@ -31,8 +35,20 @@ function addTask() {
 addButton.addEventListener("click",addTask);
 
 listContainer.addEventListener("click",function(e){
-    if (e.target.tagName ==="LI") {
-        e.target.classList.toggle("checked");
+    //FIXME: make the check box working and no line inthe midel
+    if (e.target.classList.contains("check_box")) {
+        if (e.target.classList.contains("fa-square")) {
+            e.target.classList.remove("fa-regular");
+            e.target.classList.remove("fa-square");
+            e.target.classList.add("fa-solid");
+            e.target.classList.add("fa-square-check");
+        }else{
+            e.target.classList.remove("fa-solid");
+            e.target.classList.remove("fa-square-check");
+            e.target.classList.add("fa-regular");
+            e.target.classList.add("fa-square");
+        }
+        e.target.nextElementSibling.classList.toggle("checked");
         saveData();
     }
     if(e.target.classList.contains("delete")){
